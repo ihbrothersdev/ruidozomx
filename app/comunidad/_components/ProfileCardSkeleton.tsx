0acea28b41dd1278fb38fe49436986de07504c39
Hugo Archundia <archundiah11@gmail.com>
2026-04-11 01:44:41 -0600

    feat(comunidad): rediseño de tarjetas de perfil con assets de clipboard

--- app/comunidad/_components/ProfileCardSkeleton.tsx
+++ app/comunidad/_components/ProfileCardSkeleton.tsx
@@ -1,21 +1,54 @@
+import Image from 'next/image'
 import { Skeleton } from '@/app/components/ui/skeleton'
 
 export function ProfileCardSkeleton() {
   return (
-    <div className='flex flex-col items-center'>
-      <div className='relative w-full border-2 border-black/10 bg-[#e8e2d6] shadow-md'>
-        <div className='mx-auto mt-3 aspect-[4/5] w-[85%]'>
-          <Skeleton className='h-full w-full bg-[#d4b4a8]/60' />
-        </div>
+    <div
+      className='relative w-full'
+      style={{ aspectRatio: '3 / 4' }}
+    >
+      {/* Fondo */}
+      <Image
+        src='/assets/comunidad/cards/fondo.png'
+        alt=''
+        fill
+        className='object-cover opacity-50'
+        unoptimized
+      />
 
-        <div className='flex flex-col items-center gap-2 px-3 pt-3 pb-3'>
-          <Skeleton className='h-4 w-3/4 bg-black/10' />
-          <Skeleton className='h-3 w-1/2 bg-black/10' />
-          <Skeleton className='h-3 w-2/3 bg-black/10' />
-          <Skeleton className='mt-1 h-3 w-1/2 bg-black/10' />
-          <Skeleton className='h-3 w-3/4 bg-red-600/20' />
-          <Skeleton className='mt-2 h-6 w-full bg-red-600/30' />
-        </div>
+      {/* Marco de foto skeleton */}
+      <div
+        className='absolute'
+        style={{ left: '25%', top: '6.25%', width: '49.67%', height: '46.5%' }}
+      >
+        <Skeleton className='h-full w-full bg-black/10' />
+      </div>
+
+      {/* Pinza */}
+      <Image
+        src='/assets/comunidad/cards/pinza.png'
+        alt=''
+        fill
+        className='object-cover opacity-50'
+        unoptimized
+      />
+
+      {/* Text skeletons */}
+      <div
+        className='absolute inset-x-0 flex flex-col items-center gap-[2%] px-[10%]'
+        style={{ top: '56%' }}
+      >
+        <Skeleton className='h-[3%] w-3/4 bg-black/15' style={{ minHeight: 10 }} />
+        <Skeleton className='h-[2.5%] w-1/2 bg-black/10' style={{ minHeight: 8 }} />
+        <Skeleton className='h-[2.5%] w-2/3 bg-black/10' style={{ minHeight: 8 }} />
+      </div>
+
+      {/* Button skeleton */}
+      <div
+        className='absolute inset-x-0 flex justify-center'
+        style={{ bottom: '5%' }}
+      >
+        <Skeleton className='w-[37.3%] bg-red-600/30' style={{ height: 14 }} />
       </div>
     </div>
   )
