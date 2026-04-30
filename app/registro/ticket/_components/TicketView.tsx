5c542a07232268c0a77d4655eb1c0dc4aad78a03
ivanmorales <ivann_mg@hotmail.com>
2026-04-29 19:01:23 -0600

    feat: redirect to perfil on entra

--- app/registro/ticket/_components/TicketView.tsx
+++ app/registro/ticket/_components/TicketView.tsx
@@ -146,7 +146,7 @@ function TicketContent({ isLoggedIn }: { isLoggedIn: boolean }) {
             />
           </div>
           <Link
-            href='/'
+            href='/perfil'
             className='group relative flex flex-col items-center gap-2 md:absolute md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2'
           >
             <img
