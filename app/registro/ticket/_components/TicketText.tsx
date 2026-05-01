0fa2c2f9b7c76bbbc44d01d5a7f62aeb35d02da3
ivanmorales <ivann_mg@hotmail.com>
2026-05-01 13:15:05 -0600

    fix: sizes

--- app/registro/ticket/_components/TicketText.tsx
+++ app/registro/ticket/_components/TicketText.tsx
@@ -48,7 +48,7 @@ export default function TicketText({ role, className = '', displayName = '', isL
             onClick={gated(() => setProponerRolaOpen(true))}
             className='pointer-events-auto row-span-2 flex cursor-pointer flex-col items-center justify-center px-1 lg:px-2'
           >
-            <p className='text-4xl leading-tight font-bold text-red-500 uppercase hover:underline md:text-2xl lg:text-5xl xl:text-5xl'>
+            <p className='text-3xl leading-tight font-bold text-red-500 uppercase hover:underline md:text-2xl lg:text-4xl'>
               PROPÓN UNA
             </p>
           </button>
@@ -70,10 +70,10 @@ export default function TicketText({ role, className = '', displayName = '', isL
         >
           {role === 'venue' && (
             <>
-              <p className='text-base leading-tight text-black uppercase group-hover:underline md:text-xl lg:text-3xl xl:text-4xl'>
+              <p className='text-base leading-tight text-black uppercase group-hover:underline md:text-xl lg:text-2xl'>
                 PUBLICA UNA TOCADA O
               </p>
-              <p className='text-base leading-tight text-black uppercase group-hover:underline md:text-xl lg:text-2xl xl:text-4xl'>
+              <p className='text-base leading-tight text-black uppercase group-hover:underline md:text-xl lg:text-2xl'>
                 ABRE FECHAS DISPONIBLES
               </p>
             </>
@@ -84,7 +84,7 @@ export default function TicketText({ role, className = '', displayName = '', isL
             onClick={gated(() => setProponerRolaOpen(true))}
             className='pointer-events-auto row-span-2 flex cursor-pointer flex-col items-center justify-center px-1 lg:px-2'
           >
-            <p className='text-4xl leading-tight font-bold text-red-500 uppercase hover:underline md:text-2xl lg:text-5xl xl:text-5xl'>
+            <p className='text-3xl leading-tight font-bold text-red-500 uppercase hover:underline md:text-2xl lg:text-5xl xl:text-5xl'>
               PROPÓN UNA
             </p>
           </button>
@@ -94,7 +94,7 @@ export default function TicketText({ role, className = '', displayName = '', isL
             onClick={gated(() => setProponerRolaOpen(true))}
             className='pointer-events-auto row-span-2 flex cursor-pointer flex-col items-center justify-center px-1 lg:px-2'
           >
-            <p className='text-2xl leading-tight font-bold text-red-500 uppercase hover:underline md:text-2xl lg:text-5xl xl:text-5xl'>
+            <p className='text-xl leading-tight font-bold text-red-500 uppercase hover:underline md:text-2xl lg:text-3xl xl:text-3xl'>
               PROPÓN UNA DE TUS
             </p>
           </button>
@@ -118,84 +118,84 @@ export default function TicketText({ role, className = '', displayName = '', isL
       >
         {role === 'manager' && (
           <>
-            <p className='text-7xl leading-none font-black text-red-500 uppercase group-hover:underline md:text-5xl lg:text-6xl xl:text-7xl'>
+            <p className='text-6xl leading-none font-black text-red-500 uppercase group-hover:underline md:text-5xl lg:text-6xl'>
               ROLA
             </p>
-            <p className='text-md leading-tight text-red-500 uppercase group-hover:underline md:text-sm lg:text-xl xl:text-3xl'>
+            <p className='text-md leading-tight text-red-500 uppercase group-hover:underline md:text-sm lg:text-xl'>
               Del talento que mueves
             </p>
-            <p className='text-md leading-tight text-black uppercase group-hover:underline md:text-sm lg:text-xl xl:text-3xl'>
+            <p className='text-xs leading-tight text-black uppercase group-hover:underline md:text-sm lg:text-xl'>
               para nuestro cassete semanal
             </p>
           </>
         )}
         {(role === 'promotor' || role === 'agente') && (
           <>
-            <p className='text-xl leading-tight text-red-500 uppercase group-hover:underline md:text-lg lg:text-xl xl:text-5xl'>
+            <p className='text-xl leading-tight text-red-500 uppercase group-hover:underline md:text-lg lg:text-2xl'>
               PROPÓN UNA
             </p>
-            <p className='text-6xl leading-none font-black text-red-500 uppercase group-hover:underline md:text-5xl lg:text-6xl xl:text-7xl'>
+            <p className='text-5xl leading-none font-black text-red-500 uppercase group-hover:underline md:text-5xl lg:text-6xl'>
               ROLA
             </p>
-            <p className='text-md leading-tight text-red-500 uppercase group-hover:underline md:text-sm lg:text-xl xl:text-3xl'>
+            <p className='text-md leading-tight text-red-500 uppercase group-hover:underline md:text-sm lg:text-xl'>
               Del talento que mueves
             </p>
-            <p className='text-md leading-tight text-black uppercase group-hover:underline md:text-sm lg:text-xl xl:text-3xl'>
+            <p className='text-xs leading-tight text-black uppercase group-hover:underline md:text-sm lg:text-xl'>
               para nuestro cassete semanal
             </p>
           </>
         )}
         {role === 'venue' && (
           <button className='group pointer-events-auto cursor-pointer'>
-            <p className='text-xl leading-tight text-red-500 uppercase group-hover:underline md:text-xl lg:text-xl xl:text-5xl'>
+            <p className='text-xl leading-tight text-red-500 uppercase group-hover:underline md:text-xl lg:text-2xl'>
               PROPÓN UNA
             </p>
-            <p className='text-6xl leading-none font-black text-red-500 uppercase group-hover:underline md:text-5xl lg:text-6xl xl:text-7xl'>
+            <p className='text-5xl leading-none font-black text-red-500 uppercase group-hover:underline md:text-5xl lg:text-6xl'>
               ROLA
             </p>
-            <p className='text-md leading-tight text-red-500 uppercase group-hover:underline md:text-sm lg:text-xl xl:text-3xl'>
+            <p className='text-xs leading-tight text-red-500 uppercase group-hover:underline md:text-sm lg:text-xl'>
               De tu banda o artista favorito
             </p>
-            <p className='text-md leading-tight text-black uppercase group-hover:underline md:text-sm lg:text-xl xl:text-3xl'>
+            <p className='text-xs leading-tight text-black uppercase group-hover:underline md:text-sm lg:text-xl'>
               para nuestro cassete semanal
             </p>
           </button>
         )}
         {role === 'fan' && (
           <>
-            <p className='text-7xl leading-none font-black text-red-500 uppercase group-hover:underline md:text-5xl lg:text-6xl xl:text-7xl'>
+            <p className='text-6xl leading-none font-black text-red-500 uppercase group-hover:underline sm:text-7xl md:text-5xl lg:text-6xl'>
               ROLA
             </p>
-            <p className='text-md leading-tight text-red-500 uppercase group-hover:underline md:text-sm lg:text-xl xl:text-3xl'>
+            <p className='text-xs leading-tight text-red-500 uppercase group-hover:underline sm:text-md md:text-sm lg:text-xl'>
               De tu banda o artista favorito
             </p>
-            <p className='text-md leading-tight text-black uppercase group-hover:underline md:text-sm lg:text-xl xl:text-3xl'>
+            <p className='text-xs leading-tight text-black uppercase group-hover:underline sm:text-md md:text-sm lg:text-xl'>
               para nuestro cassete semanal
             </p>
           </>
         )}
         {role === 'banda' && (
           <>
-            <p className='text-7xl leading-none font-black text-red-500 uppercase group-hover:underline md:text-5xl lg:text-6xl xl:text-9xl'>
+            <p className='text-6xl leading-none font-black text-red-500 uppercase group-hover:underline md:text-5xl lg:text-5xl xl:text-8xl'>
               ROLAS
             </p>
-            <p className='text-md text-BLACK mt-0.5 leading-tight uppercase group-hover:underline md:text-xs lg:mt-1 lg:text-xl xl:text-3xl'>
+            <p className='text-xs text-black mt-0.5 leading-tight uppercase group-hover:underline md:text-xs lg:mt-1 lg:text-xl xl:text-xl'>
               PARA NUESTRO CASSETE SEMANAL
             </p>
           </>
         )}
         {role === 'proveedor' && (
           <>
-            <p className='text-xl leading-tight text-red-500 uppercase group-hover:underline md:text-lg lg:text-xl xl:text-4xl'>
+            <p className='text-xl leading-tight text-red-500 uppercase group-hover:underline md:text-lg lg:text-2xl'>
               PROPÓN UNA
             </p>
-            <p className='text-6xl leading-none font-black text-red-500 uppercase group-hover:underline md:text-5xl lg:text-6xl xl:text-8xl'>
+            <p className='text-6xl leading-none font-black text-red-500 uppercase group-hover:underline md:text-5xl lg:text-6xl'>
               ROLA
             </p>
-            <p className='mt-0.5 text-xs leading-tight text-red-500 uppercase group-hover:underline md:text-xs lg:mt-1 lg:text-sm xl:text-xl'>
+            <p className='mt-0.5 text-xs leading-tight text-red-500 uppercase group-hover:underline md:text-xs lg:mt-1 lg:text-sm xl:text-lg'>
               De tu banda o artista favorito
             </p>
-            <p className='mt-0.5 text-xs leading-tight text-black uppercase group-hover:underline md:text-xs lg:mt-1 lg:text-sm xl:text-xl'>
+            <p className='mt-0.5 text-xs leading-tight text-black uppercase group-hover:underline md:text-xs lg:mt-1 lg:text-sm xl:text-lg'>
               para nuestro cassete semanal
             </p>
           </>
