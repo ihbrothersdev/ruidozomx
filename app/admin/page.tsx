548b74b1f9a13bdc1a7b0e509e9904301b4c860b
Hugo Archundia <archundiah11@gmail.com>
2026-06-10 23:29:15 -0600

    Revert "feat(admin): agrega pestaña de eventos en solo lectura"
    This reverts commit c432da6871943473a41774e4738b8782a143d6e5.

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -96,7 +96,6 @@ export default async function AdminDashboardPage() {
           sub='visibles al público'
           accent='amber'
           icon={CalendarDays}
-          href='/admin/eventos'
         />
       </section>
 
