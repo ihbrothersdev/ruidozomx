9bf9b39d02962c290684f0ddcd0d7224963b0328
ivanmorales <ivann_mg@hotmail.com>
2026-06-14 05:20:28 -0600

    fix: dialog bubble

--- app/components/player/DialogBubble.tsx
+++ app/components/player/DialogBubble.tsx
@@ -11,7 +11,7 @@ export function DialogBubble({ isAuthenticated }: DialogBubbleProps) {
   return (
     <Link
       href='/registro/elige-rol'
-      className='absolute top-1 left-0 z-10 w-[135px] transition-transform hover:scale-105 sm:top-2 sm:left-1 sm:w-[150px] md:w-[175px] lg:top-[20%] lg:-left-24 lg:w-[210px] lg:-translate-y-1/2 xl:-left-44 xl:w-[240px]'
+      className='absolute -top-5 left-0 z-10 w-[88px] transition-transform hover:scale-105 sm:top-2 sm:left-1 sm:w-[150px] md:w-[175px] lg:top-[20%] lg:-left-24 lg:w-[210px] lg:-translate-y-1/2 xl:-left-44 xl:w-[240px]'
     >
       <Image
         src='/assets/body1/register.png'
